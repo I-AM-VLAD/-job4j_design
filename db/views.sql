c8b0603f3086bd90352d86740f000d352620080b
I-AM-VLAD <vlad1slaw19982@gmail.com>
2023-09-03 00:24:43 +0300

    1. Представления [#504792]

---
+++ db/views.sql
@@ -0,0 +1,51 @@
+create table students (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+
+create table books (
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+
+create table orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books(id),
+    student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+
+create view show_the_number_of_authors_books
+ as
+select a.name, count(b.name) as "К-во книжек"
+	from orders as o
+    join books b on o.book_id = b.id
+    join authors a on b.author_id = a.id
+    group by (a.name)
+	having count(b.name) > 2;
+
+select * from show_the_number_of_authors_books;
