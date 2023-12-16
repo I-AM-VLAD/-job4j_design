c00db16fcc5a50d4739cf063d0f38583a4388663
I-AM-VLAD <vlad1slaw19982@gmail.com>
2023-12-16 18:54:03 +0200

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/inout/io/Analysis.java
@@ -0,0 +1,46 @@
+package ru.job4j.inout.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            read.lines().forEach(list::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(target)
+                ))) {
+            String start = null;
+            String finish = null;
+            for (int i = 0; i < list.size(); i++) {
+                String[] spl = list.get(i).split(" ");
+                if ("400".equals(spl[0]) || "500".equals(spl[0])) {
+                    start = spl[1];
+                    while (i < list.size()) {
+                       ++i;
+                       if (i < list.size()) {
+                           String[] splIn = list.get(i).split(" ");
+                           if ("200".equals(splIn[0]) || "300".equals(splIn[0])) {
+                               finish = splIn[1];
+                               break;
+                           }
+                       }
+                    }
+                    out.println(start + ";" + finish);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
