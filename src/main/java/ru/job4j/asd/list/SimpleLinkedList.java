8e8643f5a5322fc195001b7978446200829cd55f
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-01-27 18:58:04 +0200

    2. Создать контейнер на базе связанного списка [#159 #476880]

--- src/main/java/ru/job4j/asd/list/SimpleLinkedList.java
+++ src/main/java/ru/job4j/asd/list/SimpleLinkedList.java
@@ -1,11 +1,14 @@
 package ru.job4j.asd.list;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleLinkedList<E> implements SimpleLinked<E> {
 
     private int size = 0;
-    private int modCount;
+    private int modCount = 0;
     private Node<E> head = null;
     private Node<E> current = head;
 
@@ -15,6 +18,7 @@ public class SimpleLinkedList<E> implements SimpleLinked<E> {
         if (head == null) {
             head = node;
             ++size;
+            ++modCount;
         } else {
             current = head;
             while (current.next != null) {
@@ -22,17 +26,49 @@ public class SimpleLinkedList<E> implements SimpleLinked<E> {
             }
             current.next = node;
             ++size;
+            ++modCount;
         }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        E result = null;
+        if (Objects.checkIndex(index, size) == index) {
+            int localIndex = 0;
+            current = head;
+            while (localIndex != index) {
+                current = current.next;
+                ++localIndex;
+            }
+            result = current.item;
+        }
+        return result;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            int expectedModCount = modCount;
+            Node<E> currentIter = head;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return  currentIter != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> saveNode = currentIter;
+                currentIter = currentIter.next;
+                return saveNode.item;
+            }
+        };
     }
 
     private static class Node<E> {
@@ -49,6 +85,9 @@ public class SimpleLinkedList<E> implements SimpleLinked<E> {
         list.add(1);
         list.add(2);
         list.add(3);
-        list.add(4);
+        Iterator<Integer> iterator = list.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
     }
 }
