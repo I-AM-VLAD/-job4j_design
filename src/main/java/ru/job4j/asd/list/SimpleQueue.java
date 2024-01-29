415de776aadb3b6c659a80f261bed921d8fbdd0e
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-01-29 23:03:50 +0200

    5. Очередь на двух стеках [#160 #477274]

--- src/main/java/ru/job4j/asd/list/SimpleQueue.java
+++ src/main/java/ru/job4j/asd/list/SimpleQueue.java
@@ -1,10 +1,15 @@
 package ru.job4j.asd.list;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> input = new SimpleStack<>();
     private final SimpleStack<T> output = new SimpleStack<>();
     private int size = 0;
     public T poll() {
+        if (size == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
         while (size != 0) {
             output.push(input.pop());
             --size;
