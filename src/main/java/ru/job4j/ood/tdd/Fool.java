f205d3fea6bde2c7cd7a54703740f0bef2763fbb
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-02-26 12:09:07 +0200

    2. Принципы Kiss, Dry и Yagni [#238813]

--- src/main/java/ru/job4j/ood/tdd/Fool.java
+++ src/main/java/ru/job4j/ood/tdd/Fool.java
@@ -3,22 +3,24 @@ package ru.job4j.ood.tdd;
 import java.util.Scanner;
 
 public class Fool {
+    static class Answer {
+        private int number;
 
-    public static void answerComputer(int startAt) {
-        if (startAt % 3 == 0 && startAt % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (startAt % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (startAt % 5 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(startAt);
+        public Answer(int num) {
+            this.number = num;
         }
-    }
 
-    public static int message() {
-        System.out.println("Ошибка. Начинай снова.");
-        return 0;
+        public String lable() {
+            String result = String.valueOf(number);
+            if (number % 3 == 0 && number % 5 == 0) {
+                result = "FizzBuzz";
+            } else if (number % 3 == 0) {
+                result = "Fizz";
+            } else if (number % 5 == 0) {
+                result = "Buzz";
+            }
+            return result;
+        }
     }
 
     public static void main(String[] args) {
@@ -26,25 +28,11 @@ public class Fool {
         var startAt = 1;
         var input = new Scanner(System.in);
         while (startAt < 100) {
-            answerComputer(startAt);
+            System.out.println(new Answer(startAt).lable());
             startAt++;
-            var answer = input.nextLine();
-            if (startAt % 3 == 0 && startAt % 5 == 0) {
-                if (!"FizzBuzz".equals(answer)) {
-                    startAt = message();
-                }
-            } else if (startAt % 3 == 0) {
-                if (!"Fizz".equals(answer)) {
-                    startAt = message();
-                }
-            } else if (startAt % 5 == 0) {
-                if (!"Buzz".equals(answer)) {
-                    startAt = message();
-                }
-            } else {
-                if (!String.valueOf(startAt).equals(answer)) {
-                    startAt = message();
-                }
+            if (!input.nextLine().equals(new Answer(startAt).lable())) {
+                System.out.println("Ошибка");
+                startAt = 0;
             }
             startAt++;
         }
