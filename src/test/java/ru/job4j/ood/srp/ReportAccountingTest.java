faf210a6de021297e35c5b27051f5d77e77d15d2
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-03-09 18:06:41 +0200

    1. Отчеты. [#850]

--- src/test/java/ru/job4j/ood/srp/ReportAccountingTest.java
+++ src/test/java/ru/job4j/ood/srp/ReportAccountingTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.*;
 
 class ReportAccountingTest {
     @Test
-    public void whenTestGenerated () {
+    public void whenTestGenerated() {
         MemoryStore store = new MemoryStore();
         Calendar now = Calendar.getInstance();
         Employee worker1 = new Employee("Ivan1", now, now, 100);
