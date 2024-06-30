8e666fdef9e7abe75548fba721f8c2910a00e157
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-06-30 11:19:55 +0300

    3.2. Откат изменений [#504830]

---
+++ src/main/java/ru/job4j/git/Message.java
@@ -0,0 +1,15 @@
+package ru.job4j.git;
+
+public class Message {
+    public String helloWold() {
+        return "Hello World";
+    }
+
+    public String helloEveryone() {
+        return "Hello everyone";
+    }
+
+    public String hello() {
+        return "Hello";
+    }
+}
