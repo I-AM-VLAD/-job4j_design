48fc6296ce266aa3547a3b9f897bc20c37f2ccf8
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-07-07 16:55:10 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/thread/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.thread;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        try {
+            int i = 0;
+            while (!Thread.currentThread().isInterrupted()) {
+                var process = new char[] {'-', '\\', '|', '/'};
+                System.out.print("\r load: " + process[i]);
+                i++;
+                if (i == 4) {
+                    i = 0;
+                }
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+        progress.interrupt();
+    }
+}
