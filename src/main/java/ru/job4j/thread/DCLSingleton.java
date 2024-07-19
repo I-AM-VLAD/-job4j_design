55a42d52525754b94d9c5f3ba26cc1c20c49a7bb
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-07-19 22:38:43 +0300

    2. Модель памяти Java [#267917]

--- src/main/java/ru/job4j/thread/DCLSingleton.java
+++ src/main/java/ru/job4j/thread/DCLSingleton.java
@@ -1,17 +1,22 @@
 package ru.job4j.thread;
 
-public final class DCLSingleton {
-
+public class DCLSingleton {
     private static volatile DCLSingleton instance;
 
     public static DCLSingleton getInstance() {
-                if (instance == null) {
-                    instance = new DCLSingleton();
+        DCLSingleton localInstance = instance;
+        if (localInstance == null) {
+            synchronized (DCLSingleton.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    localInstance = new DCLSingleton();
+                    instance = localInstance;
                 }
-        return instance;
+            }
+        }
+        return localInstance;
     }
 
     private DCLSingleton() {
     }
-
 }
