59a03391a0c97a2fcf7da350dfd382c57e950cdf
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-08-08 20:09:52 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/thread/nonblockingalgoritm/CASCount.java
@@ -0,0 +1,19 @@
+package ru.job4j.thread.nonblockingalgoritm;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    public void increment() {
+        counter.incrementAndGet();
+    }
+
+    public int get() {
+        return counter.get();
+    }
+}
