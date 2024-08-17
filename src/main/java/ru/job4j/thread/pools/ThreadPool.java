c6ae030a3d4efaa553f921f9c603344aa807c36c
I-AM-VLAD <vlad1slaw19982@gmail.com>
2024-08-17 09:04:43 +0300

    1. Реализовать ThreadPool [#1099 #509225]

--- src/main/java/ru/job4j/thread/pools/ThreadPool.java
+++ src/main/java/ru/job4j/thread/pools/ThreadPool.java
@@ -1,29 +1,28 @@
 package ru.job4j.thread.pools;
 
+
+
 import ru.job4j.thread.notifyall.SimpleBlockingQueue;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class ThreadPool {
+
+    private final int size = Runtime.getRuntime().availableProcessors();
+
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
 
-    public ThreadPool() throws InterruptedException {
-        int size = Runtime.getRuntime().availableProcessors();
-        work(() -> System.out.println(Thread.currentThread().getName()));
-        work(() -> System.out.println(Thread.currentThread().getName()));
-        for (int i = 0; i < size; i++) {
-            threads.add((new Thread(tasks.poll())));
-        }
-    }
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
 
-    public List<Thread> getThreads() {
-        return threads;
+    public ThreadPool() {
+        this.init();
     }
 
     public void work(Runnable job) throws InterruptedException {
-        tasks.offer(job);
+        if (job != null) {
+            tasks.offer(job);
+        }
     }
 
     public void shutdown() {
@@ -32,13 +31,21 @@ public class ThreadPool {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        ThreadPool threadPool = new ThreadPool();
-        threadPool.work(() -> System.out.println(Thread.currentThread().getName()));
-        threadPool.work(() -> System.out.println(Thread.currentThread().getName()));
-        for (Thread thread : threadPool.getThreads()) {
+    private void init() {
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(
+                    () -> {
+                        try {
+                            while (!Thread.currentThread().isInterrupted()) {
+                                tasks.poll().run();
+                            }
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+            );
             thread.start();
+            threads.add(thread);
         }
-        threadPool.shutdown();
     }
 }
